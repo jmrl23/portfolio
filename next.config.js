fc7e5ea9a8304ccc26d66e824e47841d78f798cc
Jomariel Gaitera <jojogaitera@gmail.com>
2022-09-11 22:40:01 +0800

    Add API server

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.BACKEND_URL}/:path*`
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
