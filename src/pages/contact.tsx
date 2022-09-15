4de5ae7e0f4ff67ba40114973e340569983dc57e
Jomariel Gaitera <jojogaitera@gmail.com>
2022-09-15 23:40:44 +0800

    Fix layout

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -73,8 +73,8 @@ const Contact: NextPage = () => {
             </div>
             <textarea className='p-4 rounded h-[calc(100vh-17rem)] resize-none w-full bg-gray-50 dark:bg-white border-2 border-sky-400 border-dashed dark:border-none focus:outline-none' name="message" placeholder='Message'></textarea>
             <div className='flex flex-row justify-between mt-2 items-center'>
-              <span className='flex gap-x-2 dark:text-white'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <span className='flex gap-x-2 dark:text-white text-xs'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                   <path strokeLinecap="round" d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25" />
                 </svg>
                 gaiterajomariel@gmail.com
