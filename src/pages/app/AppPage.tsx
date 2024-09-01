b8e21b4d7b86ff58755d5a69c5aad6465127d78d
Jomariel Gaitera <gaiterajomariel@gmail.com>
2024-09-02 02:45:02 +0800

    refactor(content): fix navigation

--- src/pages/app/AppPage.tsx
+++ src/pages/app/AppPage.tsx
@@ -6,19 +6,45 @@ import Footer from '@/pages/app/Footer';
 import Navigation from '@/pages/app/Navigation';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+export interface Content {
+  id: string;
+  name: string;
+  element: JSX.Element;
+}
+
+const contents: Content[] = [
+  {
+    id: 'introduction',
+    name: 'Introduction',
+    element: <Introduction key={'introduction'} />,
+  },
+  {
+    id: 'technologies',
+    name: 'Technologies',
+    element: <Technologies key={'technologies'} />,
+  },
+  {
+    id: 'projects',
+    name: 'Projects',
+    element: <Projects key={'projects'} />,
+  },
+  {
+    id: 'contact',
+    name: 'Contact',
+    element: <Contact key={'contact'} />,
+  },
+];
+
 const queryClient = new QueryClient();
 
 export default function AppPage() {
   return (
     <QueryClientProvider client={queryClient}>
       <main className='text-lg'>
-        <Introduction />
-        <Technologies />
-        <Projects />
-        <Contact />
+        {contents.map((content) => content.element)}
       </main>
       <Footer />
-      <Navigation />
+      <Navigation contents={contents} />
     </QueryClientProvider>
   );
 }
--- src/pages/app/Navigation.tsx
+++ src/pages/app/Navigation.tsx
@@ -6,28 +6,28 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '@/components/ui/tooltip';
+import { Content } from '@/pages/app/AppPage';
 import { CircleDotIcon, CircleIcon } from 'lucide-react';
 import { useEffect, useState } from 'react';
 
-export default function Navigation() {
-  const [activeNav, setActiveNav] = useState<number>(0);
-  const [contents, setContents] = useState<Array<HTMLDivElement | null>>([]);
+interface Props {
+  contents: Content[];
+}
 
-  useEffect(() => {
-    setContents(
-      [
-        document.querySelector<HTMLDivElement>('#introduction'),
-        document.querySelector<HTMLDivElement>('#technologies'),
-        document.querySelector<HTMLDivElement>('#projects'),
-        document.querySelector<HTMLDivElement>('#contact'),
-      ].filter((value) => value !== null),
-    );
-  }, []);
+export default function Navigation({ contents }: Props) {
+  const [activeNav, setActiveNav] = useState<number>(0);
+  const [elements, setElements] = useState<Array<HTMLDivElement | null>>([]);
 
   useEffect(
     function () {
+      setElements(
+        contents.map((content) =>
+          document.querySelector<HTMLDivElement>(`#${content.id}`),
+        ),
+      );
+
       function onScroll() {
-        const abs = contents.map((element) =>
+        const abs = elements.map((element) =>
           Math.abs(
             element?.getBoundingClientRect().top ?? Number.MAX_SAFE_INTEGER,
           ),
@@ -43,64 +43,41 @@ export default function Navigation() {
         window.removeEventListener('scroll', onScroll);
       };
     },
-    [activeNav, contents],
+    [activeNav, contents, elements],
   );
 
   return (
     <nav className='fixed -right-2 md:right-0 top-0 lg:mr-2 h-full space-y-4 flex flex-col justify-center'>
       <ThemeToggle className='bg-transparent hover:bg-transparent' />
       <div className='text-center'>&#x2022;</div>
-      {navItems
-        .filter((item) => !!contents.some((content) => content?.id === item.id))
-        .map((item, index) => (
-          <TooltipProvider key={item.id}>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  variant={'ghost'}
-                  size={'sm'}
-                  className='bg-transparent hover:bg-transparent cursor-default md:cursor-pointer'
-                  onClick={() => {
-                    contents.at(index)?.scrollIntoView({
-                      behavior: 'smooth',
-                    });
-                  }}
-                >
-                  {activeNav === index ? (
-                    <CircleDotIcon className='w-4 h-4 lg:w-6 lg:h-6 cursor-default md:cursor-pointer text-orange-500' />
-                  ) : (
-                    <CircleIcon className='w-4 h-4 lg:w-6 lg:h-6 cursor-default md:cursor-pointer hover:text-sky-500' />
-                  )}
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent>{item.name}</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        ))}
+      {contents.map(
+        (content, index) =>
+          elements.at(index) && (
+            <TooltipProvider key={content.id}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    variant={'ghost'}
+                    size={'sm'}
+                    className='bg-transparent hover:bg-transparent cursor-default md:cursor-pointer'
+                    onClick={() => {
+                      document.getElementById(content.id)?.scrollIntoView({
+                        behavior: 'smooth',
+                      });
+                    }}
+                  >
+                    {activeNav === index ? (
+                      <CircleDotIcon className='w-4 h-4 lg:w-6 lg:h-6 cursor-default md:cursor-pointer text-orange-500' />
+                    ) : (
+                      <CircleIcon className='w-4 h-4 lg:w-6 lg:h-6 cursor-default md:cursor-pointer hover:text-sky-500' />
+                    )}
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>{content.name}</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          ),
+      )}
     </nav>
   );
 }
-
-interface NavItem {
-  id: string;
-  name: string;
-}
-
-const navItems: NavItem[] = [
-  {
-    id: 'introduction',
-    name: 'Introduction',
-  },
-  {
-    id: 'technologies',
-    name: 'Technologies',
-  },
-  {
-    id: 'projects',
-    name: 'Projects',
-  },
-  {
-    id: 'contact',
-    name: 'Contact',
-  },
-];
